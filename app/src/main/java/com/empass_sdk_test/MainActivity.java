b40aff28616fec82406129b09a04a551279f34ce
testingempass <error.empass@gmail.com>
2017-02-14 17:22:51 +0530

    commit testing empass

--- app/src/main/java/com/empass_sdk_test/MainActivity.java
+++ app/src/main/java/com/empass_sdk_test/MainActivity.java
@@ -3,15 +3,10 @@ package com.empass_sdk_test;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.empass.sdk.main.Empass;
+import com.mylibrary.LibActivity;
+
 
 public class MainActivity extends Activity {
     Context context;
@@ -24,6 +19,7 @@ public class MainActivity extends Activity {
     }
 
     public void onClick(View view) {
-        Empass.initializeEmpass(context, "amifdkaf9ojasdf09ausdjad", "amity", "52.25.188.36");
+        LibActivity.callLib(this);
     }
+
 }
